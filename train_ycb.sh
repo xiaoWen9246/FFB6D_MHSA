645476017f12d8efc0729bb40e20a75de207f7b7
xiaoWen <106232467+xiaoWen9246@users.noreply.github.com>
2023-12-07 17:44:58 +0800

    Delete train_ycb.sh

--- train_ycb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-n_gpu=6  # number of gpu to use
-tst_mdl=train_log/ycb/checkpoints/FFB6D.pth.tar  # checkpoint to train
-python -m torch.distributed.launch --nproc_per_node=$n_gpu --master_port 47769 train_ycb.py --gpus=$n_gpu -checkpoint=$tst_mdl
