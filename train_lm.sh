b8e301e618b047709838df61e65afa0f32c998e1
xiaoWen <106232467+xiaoWen9246@users.noreply.github.com>
2023-12-07 17:44:41 +0800

    Delete train_lm.sh

--- train_lm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-n_gpu=6
-cls='iron'
-checkpoint=train_log/linemod/checkpoints/${cls}/FFB6D_${cls}.pth.tar
-python3 -m torch.distributed.launch --nproc_per_node=$n_gpu train_lm.py --gpus=$n_gpu --cls=$cls 
